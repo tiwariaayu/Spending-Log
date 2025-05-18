c21bf689a60660377dd28bd8457e2850aa608985
tiwariaayu <tiwariayushman847@gmail.com>
2025-05-18 12:11:51 +0530

    Frontend commit

---
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncomeOverview = ({transaction, onAddIncome}) => {
+  return (
+    <div>IncomeOverview</div>
+  )
+}
+
+export default IncomeOverview
---
+++ frontend/expense-tracker/src/utils/helper.js
@@ -0,0 +1,37 @@
+export const validateEmail = (email) => {
+    const regex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/ ;
+    return regex.test(email);
+};
+
+export const getInitials = (name) => {
+    if (!name) return "";
+
+    const words = name.split(" ");
+    let initials = "";
+
+    for ( let i = 0 ; i<Math.min(words.length, 2);i++){
+        initials += words[i][0];
+    }
+
+    return initials.toUpperCase();
+};
+
+export const addThousandSeparator = (num) => {
+    if(num == null || isNaN(num)) return "";
+
+    const [integerPart, fractionalPart] = num.toString().split(".");
+    const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return fractionalPart 
+     ? `${formattedIntegerPart}.${fractionalPart}`
+     : formattedIntegerPart;
+};
+
+export const prepareExpenseBarChartData = (data =[])=> {
+    const charData= data.map((item)=> ({
+        category: item?.category,
+        amount:item?.amount,
+    }));
+
+    return charData;
+}
